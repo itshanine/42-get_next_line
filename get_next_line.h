f40e6f0974e9280098a933c2271cb73a5159946d
hanine <hanine@macbook-air.home>
2023-10-11 01:16:18 +0200

    added files

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanjebou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 00:49:11 by hanjebou          #+#    #+#             */
+/*   Updated: 2023/10/11 01:10:03 by hanjebou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+//get_next_line_utils
+int		ft_strlen(char *s);
+char	*ft_strdup(char *s);
+
+//get_next_line
+
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanjebou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 00:44:05 by hanjebou          #+#    #+#             */
+/*   Updated: 2023/10/11 01:10:33 by hanjebou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_utils.h"
+
+int	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t	i;
+	int		size;
+	char	*dup;
+
+	size = ft_strlen(s);
+	dup = malloc(sizeof(char) * (size + 1));
+	if (!dup)
+		return (NULL);
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
